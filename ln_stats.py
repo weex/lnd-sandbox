5d234005252d06970093f43237fee8e14da4d696
David Sterry <davids@exchb.com>
2019-01-04 01:40:27 +0000

    more robust for prod

--- ln_stats.py
+++ ln_stats.py
@@ -4,47 +4,51 @@
 
 from lnd_rpc import LNDRPC
 from time import sleep, gmtime, strftime
+import subprocess
 import json
 import re
 from math import log
 
-l = LNDRPC()
+#l = LNDRPC()
 
 # get graph
-g = l.describe_graph()
+#g = l.describe_graph()
+
+
+g = json.loads(subprocess.check_output(['/home/dsterry/gocode/bin/lncli', 'describegraph']))
 
 total_capacity = 0
-for e in g.edges:
-    total_capacity += e.capacity
+for e in g['edges']:
+    total_capacity += float(e['capacity'])
 
-channels = len(g.edges)
-nodes = len(g.nodes)
+channels = len(g['edges'])
+nodes = len(g['nodes'])
 
 # check aliases for uniqueness
 aliases = {}
-for n in g.nodes:
-    if n.alias not in aliases:
-        aliases[n.alias] = 1
+for n in g['nodes']:
+    if n['alias'] not in aliases:
+        aliases[n['alias']] = 1
     else:
-        aliases[n.alias] += 1
+        aliases[n['alias']] += 1
 
 node_ids = {}
-for n in g.nodes:
+for n in g['nodes']:
     # use alias if unique, otherwise pub_key
-    if aliases[n.alias] == 1:
-        node_ids[n.pub_key] = n.alias.rstrip(' \t\r\n\0')
+    if aliases[n['alias']] == 1:
+        node_ids[n['pub_key']] = n['alias'].rstrip(' \t\r\n\0')
     else:
-        node_ids[n.pub_key] = n.pub_key
+        node_ids[n['pub_key']] = n['pub_key']
 
 j_nodes = []
-for n in g.nodes:
-    j_nodes.append({"id": node_ids[n.pub_key], "group":n.color})
+for n in g['nodes']:
+    j_nodes.append({"id": node_ids[n['pub_key']], "group":n['color']})
 
 j_edges = []
-for e in g.edges:
-    j_edges.append({"source": node_ids[e.node1_pub],
-                    "target": node_ids[e.node2_pub],
-                    "value": int(50/log(e.capacity))})
+for e in g['edges']:
+    j_edges.append({"source": node_ids[e['node1_pub']],
+                    "target": node_ids[e['node2_pub']],
+                    "value": int(50/log(float(e['capacity'])))})
 
 json_out = {"nodes": j_nodes, "links": j_edges}
 print json.dumps(json_out)
